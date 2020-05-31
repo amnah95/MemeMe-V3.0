fc9932d1af0876e405dd18530dba2d90c5096f55
amnah95 <34598209+amnah95@users.noreply.github.com>
2020-05-31 18:00:02 +0300

    Added view controllers

--- MemeMe-V1.0/ViewController.swift
+++ MemeMe-V1.0/ViewController.swift
@@ -18,11 +18,42 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    // Prompts the user to pick an image from the photo album
+    // Prompts user to take new image
+    @IBAction func takeNewImage(_ sender: UIButton) {
+    }
+        
+    // Prompts user to pick image from album
     @IBAction func pickAnImage(_ sender: Any) {
         let imagePicker = UIImagePickerController()
         //imagePicker.delegate = self
         present(imagePicker, animated: true, completion: nil)
+    }
+    
+    // Prompts user to take new image
+    @IBAction func shareMeme (_ sender: Any) {
+        let memeImage = UIImage()
+        let shareMemeViewController = UIActivityViewController(activityItems: [memeImage], applicationActivities: nil)
+        present(shareMemeViewController, animated: true, completion: nil)
+    }
+    
+    // to cancel meme created
+    @IBAction func cancelMeme (_ sender: Any) {
+        let cancelMemeAlretContorller = UIAlertController()
+        cancelMemeAlretContorller.title = "You are discarding current meme."
+        cancelMemeAlretContorller.message = "Sure you want to continue?"
+        
+        let sureAction = UIAlertAction(title: "Sure", style: UIAlertAction.Style.default) { action in print("Sure is clicked")
+        }
+        
+        let noAction = UIAlertAction(title: "No", style: UIAlertAction.Style.default) { action in self.dismiss(animated: true, completion: nil)
+        }
+            cancelMemeAlretContorller.addAction(sureAction)
+            cancelMemeAlretContorller.addAction(noAction)
+        
+        present(cancelMemeAlretContorller, animated: true, completion: nil)
+        
+    }
+    
         
     }
     
@@ -31,6 +62,3 @@ class ViewController: UIViewController {
 //
 //    func imagePickerControllerDidCancel(_:)
         
-    
-}
-
