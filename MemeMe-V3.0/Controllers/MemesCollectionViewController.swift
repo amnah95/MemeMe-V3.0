11604fdc6b75b51d4e343972088f8d605719ded6
amnah95 <34598209+amnah95@users.noreply.github.com>
2020-09-06 19:42:20 +0300

    Link collectionViewController to database

--- MemeMe-V3.0/Controllers/MemesCollectionViewController.swift
+++ MemeMe-V3.0/Controllers/MemesCollectionViewController.swift
@@ -7,18 +7,16 @@
 //
 
 import UIKit
+import CoreData
 
 class MemesCollectionViewController: UICollectionViewController {
     
     var dataController: DataController!
+    var fetchedResultsController: NSFetchedResultsController<UserMeme>!
+
     
     @IBOutlet weak var flowLayout: UICollectionViewFlowLayout!
 
-    // Computed variable of the saved memes
-    var memes: [Meme]! {
-        return MemesLibrary.shared.memes
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -31,24 +29,39 @@ class MemesCollectionViewController: UICollectionViewController {
         flowLayout.minimumLineSpacing = space
         flowLayout.itemSize = CGSize(width: widthDimension, height: heightDimension)
         
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        setupFetchResultsController()
+        
         navigationItem.rightBarButtonItem = UIBarButtonItem(image: .add, style:.plain, target: self, action: #selector(createNewMeme))
       
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        collectionView.reloadData()
+        setupFetchResultsController()
+
     }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        // set fetched results controller to nil
+        fetchedResultsController = nil
+    }
+    
+    
     
     // Presenting the meme editor controller
     @objc func createNewMeme () {
         if let navigationController = navigationController {
-            let memeViewController = self.storyboard!.instantiateViewController(withIdentifier: "MemeEditorViewController") as! MemeEditorViewController
+            let memeEditorViewController = self.storyboard!.instantiateViewController(withIdentifier: "MemeEditorViewController") as! MemeEditorViewController
             
-            memeViewController.modalPresentationStyle = .fullScreen
+            memeEditorViewController.dataController = self.dataController
+            memeEditorViewController.modalPresentationStyle = .fullScreen
 
             
-            navigationController.present(memeViewController, animated: true, completion: nil)
+            navigationController.present(memeEditorViewController, animated: true, completion: nil)
         }
         
     }
@@ -57,11 +70,13 @@ class MemesCollectionViewController: UICollectionViewController {
 
 extension MemesCollectionViewController {
     
-    
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return fetchedResultsController.sections?.count ?? 1
+    }
     
     // Implement the number of collection cells method
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return memes.count
+        return fetchedResultsController.sections?[section].numberOfObjects ?? 0
     }
     
     // Implement populating cells method
@@ -69,9 +84,9 @@ extension MemesCollectionViewController {
         
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: kCollectionViewCellID, for: indexPath) as! MemeCollectionViewCell
         
-        let meme = self.memes[(indexPath as NSIndexPath).row]
+        let meme = fetchedResultsController.object(at: indexPath)
         
-        cell.memeView?.image = meme.memedImage
+        cell.memeView?.image = UIImage(data: meme.image!)
         
         return cell
     }
@@ -81,10 +96,58 @@ extension MemesCollectionViewController {
         
         let viewMemeController = self.storyboard!.instantiateViewController(withIdentifier: "MemeViewerController") as! MemeViewerController
                         
-        //viewMemeController.memeToPresent 
+        viewMemeController.memeToPresent = fetchedResultsController.object(at: indexPath)
         
         self.navigationController!.pushViewController(viewMemeController, animated: true)
     }
+}
 
+
+extension MemesCollectionViewController: NSFetchedResultsControllerDelegate {
+    
+    // Loading exsiting data in data base
+    fileprivate func setupFetchResultsController() {
+        
+        print("Fetching Photos from data base")
+        
+        // Assign Fetch request first
+        let fetchRequest: NSFetchRequest<UserMeme> = UserMeme.fetchRequest()
+        let sortDescriptor = NSSortDescriptor(key: "creationDate", ascending: false)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        
+        // Assign Fetch results controller
+        fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: dataController.viewContext, sectionNameKeyPath: nil, cacheName: "userMemes")
+        
+        // Set fectch controller delegate
+        fetchedResultsController.delegate = self
+        
+        // Perform Fetch
+        do {
+            try fetchedResultsController.performFetch()
+            print("Fetching is done, number of objects: \(fetchedResultsController.fetchedObjects?.count ?? 0)")
+        } catch {
+            print("The fetch could not be performed")
+            fatalError("The fetch could not be performed: \(error.localizedDescription)")
+        }
+    }
+    
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        
+        print("NS Controller for insert")
+        
+        switch type {
+        case .insert:
+            collectionView.insertItems(at: [newIndexPath!])
+            break
+        case .delete:
+            collectionView.deleteItems(at: [indexPath!])
+            break
+        case .update:
+            collectionView.reloadItems(at: [indexPath!])
+        default:
+            break
+        }
+    }
     
 }
